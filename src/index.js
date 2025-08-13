724586d7280e3c5c4077fdb5127519ea711bc79c
Elango8109 <elango8109@gmail.com>
2025-08-13 08:51:10 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const getItems = require('./routes/getItems');
 const addItem = require('./routes/addItem');
 const updateItem = require('./routes/updateItem');
 const deleteItem = require('./routes/deleteItem');
+const maindb= require('./persistence');
 
 app.use(express.json());
 app.use(express.static(__dirname + '/static'));
